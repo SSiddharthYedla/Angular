a0767acf451cad34c775d33c052935ef36e7792f
Shiva Siddharth Yedla <shivasiddharthyedla@gmail.com>
2026-01-08 20:12:43 -0500

    Javascript/day13/calculator_test.js 	Javascript/day13/callback_hell.js 	Javascript/day13/event_loop_1.js 	Javascript/day13/event_loop_2.js 	Javascript/day13/event_loop_3.js 	Javascript/day13/math.js 	Javascript/day13/math_test.js 	Javascript/day13/readfile_demo_1.js

---
+++ Javascript/day13/calculator.js
@@ -0,0 +1,9 @@
+export function add(a, b) {
+    console.log(`addition of ${a} and ${b} is ${a + b}`)
+}
+export function sub(a, b) {
+    console.log(`subtraction of ${a} and ${b} is ${a - b}`)
+}
+export function mul(a, b) {
+    console.log(`multiplication of ${a} and ${b} is ${a * b}`)
+}
