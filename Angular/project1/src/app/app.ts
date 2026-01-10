f0ee30ce4e33606c1915a2dd142582dcf663b9fc
Shiva Siddharth Yedla <shivasiddharthyedla@gmail.com>
2026-01-10 16:23:10 -0500

    Flipkart Website

--- Angular/project1/src/app/app.ts
+++ Angular/project1/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Body } from './body/body';
+import { Footer } from './footer/footer';
+import { Carousel } from './carousel/carousel';
+import { Categories } from './categories/categories';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navbar,Body,Carousel,Footer,Categories,Component],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
